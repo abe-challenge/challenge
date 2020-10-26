c0d7af12b5e83c4ddae7c1cf6e342a50ddbad1c2
abe-challenge <asilelik@gmail.com>
2020-10-27 00:01:09 +0100

    solution structure

--- docker/init.sql
+++ docker/init.sql
@@ -2,11 +2,14 @@ SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+USE `challenge`;
+
 SET NAMES utf8mb4;
 
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `name` char(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `price` int NOT NULL,
   `stock` int NOT NULL DEFAULT '0',
--- public/index.php
+++ public/index.php
@@ -8,26 +8,15 @@ if (preg_match('/\.(?:js|html)$/', $requestPath)) {
     exit;
 }
 
-echo '<pre>';
-var_dump(1);
-exit;
-use DI\Container;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$container = new Container();
-AppFactory::setContainer($container);
 $app = AppFactory::create();
 
-// $container->set('myService', function () {
-//     $settings = [...];
-//     return new MyService($settings);
-// });
-$app->addErrorMiddleware(true, false, false);
-$app->get('/', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
+$app->get('/api/products', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
 });
